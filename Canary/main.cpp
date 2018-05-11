af321fefdf2b720bac2c34a825b97674e6e6cffb
C-BeatsPC\C <dimichelec@gmail.com>
2018-05-11 09:57:52 -0400

    no message

--- Canary/main.cpp
+++ Canary/main.cpp
@@ -1,6 +1,4 @@
-﻿//#define SIM 
-#define TESTER
-
+﻿
 #define F_CPU 1000000UL
 
 #include <avr/io.h>
@@ -11,21 +9,9 @@
 
 #include "canary.h"
 
-#ifdef SIM
-	volatile bool hd_led_changed = true;			// LED changed flag
-	volatile float prescaler_freq_ms;				// prescaler configuration for WDT frequency in milliseconds
-	volatile float configured_delay;				// delay from switches scaled appropriately
-	volatile unsigned long wdt_counter = 0;			// counts number of times WDT got hit
-	volatile unsigned long main_loop_counter = 0;	// counts main loop iterations for testing
-	volatile unsigned int idle_multiplier = 60000;  // minutes for board shorter for sim
-#else
-	bool hd_led_changed = true;
-	float prescaler_freq_ms;
-	float configured_delay;
-	unsigned long wdt_counter = 0;
-	unsigned long main_loop_counter = 0;
-	unsigned int idle_multiplier = 60000;
-#endif
+
+bool hd_led_changed = true;
+
 
 void delay_ms(unsigned long ms)
 {
@@ -45,76 +31,28 @@ void tester_flash(int times, int howlong) {
 }
 
 
-bool is_led_changed() {
-#ifdef SIM
-	volatile bool is_changed = readbit(GIFR,PCIF);
-#else
-	bool is_changed = readbit(GIFR,PCIF);
-#endif
-
-	if (is_changed) { setbit(GIFR,PCIF); }
-	
-	return is_changed;
-}
-
-
 ISR(PCINT0_vect) {
+	clrbit(PCMSK, PCINT4);
 	hd_led_changed = true;
+	tester_flash(1,FLASH_DELAY_SHORT_MS);
 }
 
 ISR(WDT_vect) {
-	wdt_counter++;
 	setbit(WDTCR, WDIE);	// this keeps us from resetting the micro
-#ifdef TESTER
 	tester_flash(1,FLASH_DELAY_LONG_MS);
-#endif
+	if (hd_led_changed) {
+		tester_flash(1,FLASH_DELAY_SHORT_MS);
+		tester_flash(1,FLASH_DELAY_SHORT_MS);
+		hd_led_changed = false;
+		setbit(PCMSK, PCINT4);
+	}
 }
 
 
-// the idle time input is on DIP switches #1-3, is logically inverted, and rolled.
-unsigned char idletime_input() {
-#ifdef SIM
-	volatile unsigned char out;
-#else
-	unsigned char out;
-#endif
-
-	out = ~PINB & 0b00000111;
-	return (((out & 1) ? 4:0) + (out & 2) + ((out & 4) ? 1:0) + 1);
-}
-
-
-unsigned int setup_wdtcr() {
-	//set watchdog timeout to 8 seconds (max on ATtiny)
-	//datasheet 8.5.2
-	//
-	// WDIF - watchdog timeout interrupt flag
-	// WDIE - watchdog timeout interrupt enable
-	// WDCE - watchdog change enable
-	// WDE  - watchdog enable
-	// WDP3:WDP0 - timer prescaler oscillator cycles select
-	
-	//volatile unsigned char WDTCR_mask = bitval(WDIF) | bitval(WDCE);
-	//volatile unsigned char WDTCR_new = (WDTCR & WDTCR_mask) | (~WDTCR_mask | bitval(WDE) | bitval(WDIE));
-		
-	#ifdef SIM
-		volatile unsigned long prescaler_freq_ms;
-		volatile unsigned char timeout;
-		prescaler_freq_ms = 32;
-		idle_multiplier = 5;
-		timeout = WDT_TIMEOUT_32MS
-	#else
-		unsigned long prescaler_freq_ms;
-		unsigned char timeout;
-		prescaler_freq_ms = 8000;
-		idle_multiplier = 60000;
-		timeout = WDT_TIMEOUT_8S;
-	#endif
-
+void setup_wdtcr() {
+	unsigned char timeout;
+	timeout = WDT_TIMEOUT_8S;
 	WDTCR |= bitval(WDE) | bitval(WDIE) | timeout;
-	
-	return prescaler_freq_ms;
-
 }
 
 void go_to_sleep() {
@@ -123,16 +61,6 @@ void go_to_sleep() {
 	sei();							// enable interrupts
 	sleep_cpu();					// sleep
 	clrbit(MCUCR,SE);				// sleep disable
-}
-
-void reset_mobo() {
-	mobo_reset_on();
-	delay_ms(POWER_CYCLE_HOLD_MS);
-	mobo_reset_off();
-	delay_ms(POWER_CYCLE_RELEASE_MS);
-	mobo_reset_on();
-	delay_ms(POWER_CYCLE_ON_MS);
-	mobo_reset_off();
 }
 
 
@@ -154,34 +82,17 @@ int main(void)
 
 	clrbit(ADCSRA,ADEN);	// disable ADC (default is enabled in all sleep modes)
 	
-	prescaler_freq_ms = setup_wdtcr();
+	setup_wdtcr();
 
-	configured_delay = (idletime_input() * idle_multiplier) / prescaler_freq_ms;
-	
 	// we don't want to interrupt on a pin change, only check the PCIF when we
 	// come out of sleep from a watchdog timeout
-	//setbit(GIMSK, PCIE);	// enable pin change interrupts
+	setbit(GIMSK, PCIE);	// enable pin change interrupts
 	setbit(PCMSK, PCINT4);	// setup to interrupt on pin change of PB4
+
+	tester_flash(1,FLASH_DELAY_SHORT_MS);
 	
     while (1) 
     {
 		go_to_sleep();
-		
-#ifdef TESTER
-		tester_flash(1,FLASH_DELAY_SHORT_MS);
-#endif
-
-		cli();	// disable interrupts
-
-		main_loop_counter++;
-		if (is_led_changed()) {
-#ifdef TESTER
-			tester_flash(2,FLASH_DELAY_LONG_MS);
-#endif
-			wdt_counter = 0;
-		} else if (configured_delay < wdt_counter) {
-			reset_mobo();
-			wdt_counter = 0;
-		}
 	}
 }
